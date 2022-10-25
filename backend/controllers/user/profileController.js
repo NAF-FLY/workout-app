11228e3b91c3e4ba2be01b26e15ebcede7dd2dc6
Vadim <vadim192000@gmail.com>
2022-10-25 15:29:56 +0300

    Написан контроллер для получения статистики

--- backend/controllers/user/profileController.js
+++ backend/controllers/user/profileController.js
@@ -1,11 +1,37 @@
 import User from "../../models/userModel.js";
+import ExerciseLog from "../../models/exerciseLogModel.js";
 import asyncHandler from "express-async-handler";
+import WorkoutLog from "../../models/workoutLogModel.js";
 
 // @desc   Get user profile
 // @route  GET /api/users/profile
 // @access Private
 
 export const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id).select("-password");
-  res.json(user);
+  const user = await User.findById(req.user._id).select("-password").lean();
+
+  const exerciseLogByUser = await ExerciseLog.find({
+    user: user._id,
+    completed: true,
+  });
+
+  let countExerciseTimesCompleted = 0;
+  const kgs = 0;
+
+  exerciseLogByUser.forEach((log) => {
+    countExerciseTimesCompleted += log.times.length;
+
+    log.times.forEach((item) => {
+      kgs += item.weight;
+    });
+  });
+
+  const minutes = Math.ceil(countExerciseTimesCompleted * 2.3);
+
+  const workouts = await WorkoutLog.find({
+    user: user._id,
+    completed: true,
+  }).countDocuments();
+
+  res.json({ ...user, minutes, workouts, kgs });
 });
