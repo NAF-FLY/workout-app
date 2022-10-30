5f74dabcc11941293af0932a857d3a4ef5e1bd6c
Vadim <vadim192000@gmail.com>
2022-10-30 23:00:34 +0300

    на половину сделана страница с добавлением тренировки

--- client/src/components/common/Layout.jsx
+++ client/src/components/common/Layout.jsx
@@ -4,7 +4,10 @@ import styles from './Layout.module.scss'
 
 const Layout = ({ children, bgImage, height = '350px' }) => {
 	return (
-		<div className={styles.wrapper} style={{ height, backgroundImage: `url(${bgImage})` }}>
+		<div
+			className={styles.wrapper}
+			style={{ height, backgroundImage: `url(${bgImage})` }}
+		>
 			<Header />
 			{children}
 		</div>
--- client/src/components/pages/NewWorkout/NewWorkout.jsx
+++ client/src/components/pages/NewWorkout/NewWorkout.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Layout from '../../common/Layout'
+
+import bgImage from '../../../images/new-workout-bg.jpg'
+import Field from '../../ui/Field/Field'
+import Button from '../../ui/Button/Button'
+
+import styles from './NewWorkout.module.scss'
 
 const NewWorkout = () => {
+	const [name, setName] = useState('')
+
+	const handleSubmit = () => {
+		console.log('submit')
+	}
+
 	return (
-		<div>NewWorkout</div>
+		<>
+			<Layout bgImage={bgImage} />
+			<div className={styles.wrapper}>
+				<form onSubmit={handleSubmit}>
+					<Field
+						placeholder='Enter name'
+						value={name}
+						onChange={e => setName(e.target.value)}
+					/>
+					{/* {React Select} */}
+					<Button text='Create' callback={() => {}} />
+				</form>
+			</div>
+		</>
 	)
 }
 
