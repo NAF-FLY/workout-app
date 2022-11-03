333750ef8e04a508bd65249f6d672ab749aceb32
Vadim <vadim192000@gmail.com>
2022-11-03 15:45:46 +0300

    доделана страница с добавлением тренировки

--- client/src/components/pages/NewWorkout/NewWorkout.jsx
+++ client/src/components/pages/NewWorkout/NewWorkout.jsx
@@ -6,19 +6,64 @@ import Layout from '../../common/Layout'
 import bgImage from '../../../images/new-workout-bg.jpg'
 import Field from '../../ui/Field/Field'
 import Button from '../../ui/Button/Button'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { $api } from '../../../api/api'
+import Alert from '../../ui/Alert/Alert'
+import Loader from '../../ui/Loader'
 
 const NewWorkout = () => {
 	const [name, setName] = useState('')
-	const [exercises, setExercises] = useState([])
+	const [exercisesCurrent, setExercisesCurrent] = useState([])
 
-	const handleSubmit = () => {
-		console.log('submit')
+	const { data, isSuccess } = useQuery(
+		['list exercise'],
+		() =>
+			$api({
+				url: '/exercises',
+			}),
+		{
+			refetchOnWindowFocus: false,
+		}
+	)
+
+	const {
+		mutate,
+		isLoading,
+		isSuccess: isSuccessMutate,
+		error,
+	} = useMutation(
+		['Create new workout'],
+		exIds =>
+			$api({
+				url: '/workouts',
+				type: 'POST',
+				body: { name, exerciseIds: exIds },
+			}),
+		{
+			onSuccess() {
+				setName('')
+				setExercisesCurrent([])
+			},
+		}
+	)
+
+	const handleSubmit = e => {
+		e.preventDefault()
+
+		const exIds = exercisesCurrent.map(ex => ex.value)
+
+		mutate({
+			exIds,
+		})
 	}
 
 	return (
 		<>
 			<Layout bgImage={bgImage} heading='Create New Workout' />
 			<div className='wrapper-inner-page'>
+				{error && <Alert type='error' text={error} />}
+				{isSuccessMutate && <Alert text='Workout created' />}
+				{isLoading && <Loader />}
 				<form onSubmit={handleSubmit}>
 					<Field
 						placeholder='Enter name'
@@ -29,18 +74,21 @@ const NewWorkout = () => {
 					<Link to='/new-exercise' className='dark-link'>
 						Add new exercise
 					</Link>
-					<ReactSelect
-						classNamePrefix='select2-selection'
-						placeholder='Exercises...'
-						title='Exercises'
-						options={[
-							{ value: 'sadfasdf', label: 'Push-ups' },
-							{ value: 'dsfasdaa', label: 'Pull-ups' },
-						]}
-						value={exercises}
-						onChange={setExercises}
-						isMulti={true}
-					></ReactSelect>
+					{isSuccess && data && (
+						<ReactSelect
+							classNamePrefix='select2-selection'
+							placeholder='Exercises...'
+							title='Exercises'
+							options={[
+								{ value: 'sadfasdf', label: 'Push-ups' },
+								{ value: 'dsfasdaa', label: 'Pull-ups' },
+							]}
+							value={exercisesCurrent}
+							onChange={setExercisesCurrent}
+							isMulti={true}
+						></ReactSelect>
+					)}
+
 					<Button text='Create' callback={() => {}} />
 				</form>
 			</div>
